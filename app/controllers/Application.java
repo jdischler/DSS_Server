8921f385ed42c527bd20753e2a6a4f6de8d785b9
Jeffrey Dischler <jdischler.72@gmail.com>
2013-05-30 20:42:55 -0500

    Add server side of layer parm request
    Allows client side to query server for any layer parms that may want to
    be displayed on the client

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -32,4 +32,29 @@ public class Application extends Controller {
 		String urlPath = query.exec(request().body().asJson());
 		return ok(urlPath);
 	}
+
+	//--------------------------------------------------------------------------
+	public static Result layerParmRequest() throws Exception {
+		
+		JsonNode request = request().body().asJson();
+		
+		ObjectNode ret = JsonNodeFactory.instance.objectNode();
+		
+		String layername = request.get("name").getTextValue();
+		String type = request.get("type").getTextValue();
+
+		Layer_Base layer = Layer_Base.getLayer(layername);
+		if (layer != null && type != null) {
+			if (type.equals("layerRange")) {
+				Layer_Continuous layerCont = (Layer_Continuous)layer;
+				if (layerCont == null) {
+					throw new Exception();
+				}
+				ret.put("layerMin", layerCont.getLayerMin());
+				ret.put("layerMax", layerCont.getLayerMax());
+			}
+		}
+
+		return ok(ret);
+	}
 }

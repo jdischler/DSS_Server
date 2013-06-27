c012bf3b0748334a6df6739b28124d5baaed6c7d
Jeffrey Dischler <jdischler.72@gmail.com>
2013-06-27 13:16:43 -0500

    Add wmsRequest url
    Allows watershed layer to make click feature requests

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,7 @@ import util.*;
 
 import java.util.*;
 import java.io.*;
+import java.net.*;
 
 import play.*;
 import play.mvc.*;
@@ -45,6 +46,68 @@ public class Application extends Controller
 		}
 		
 		return ok(); // derp, not really OK if gets here...or??
+	}
+	
+	//----------------------------------------------------------------------
+	public static Result wmsRequest() {
+		
+		Logger.info("Entering wmsRequest!");
+		
+		// Open up request from client...
+		JsonNode request = request().body().asJson();
+
+		Logger.info(request.toString());
+		// e.g., 'Vector:Watersheds-C'
+		String layerName = request.get("layer").getTextValue();
+		int x = request.get("x").getIntValue(); // 585
+		int y = request.get("y").getIntValue(); // 273
+		int width = request.get("width").getIntValue();
+		int height = request.get("height").getIntValue();
+		String bbox = request.get("bbox").getTextValue();
+
+		BufferedReader rd = null;
+		OutputStreamWriter wr = null;
+		
+		try {
+			URL url = new URL("http://pgis.glbrc.org:8080/geoserver/Vector/wms" + 
+				"?LAYERS=" + layerName + "&QUERY_LAYERS=" + layerName + 
+				"&STYLES=&SERVICE=WMS&VERSION=1.1.1&SRS=EPSG:900913" +
+				"&REQUEST=GetFeatureInfo&FEATURE_COUNT=10&INFO_FORMAT=application/vnd.ogc.gml/3.1.1" +
+				"&BBOX=" + bbox +
+				"&HEIGHT=" + Integer.toString(height) + 
+				"&WIDTH=" + Integer.toString(width) +
+				"&X=" + Integer.toString(x) + 
+				"&Y=" + Integer.toString(y));
+		
+			Logger.info("------------------------------");
+			URLConnection conn = url.openConnection();
+			conn.setDoOutput(true);
+			
+			wr = new OutputStreamWriter(conn.getOutputStream());
+			wr.flush();
+			
+			// get the response
+			rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			
+			String line = rd.readLine();
+			if (line != null) {
+				Logger.info(line);
+				String line1 = rd.readLine();
+				if (line1 != null) {
+					return ok(line1);
+				}
+			}
+			
+			wr.close();
+			rd.close();
+		}
+		catch (Exception e) {
+			Logger.info(e.toString());
+		}
+		
+		Logger.info("Failed");
+		return ok();//redirect("http://pgis.glbrc.org:8080/geoserver/Vector/wms?LAYERS=Vector:Watersheds-C&QUERY_LAYERS=Vector:Watersheds-C&STYLES=&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&BBOX=-10066101.81001,5298351.853223,-9874703.49121,5394968.256962&FEATURE_COUNT=10&HEIGHT=632&WIDTH=1252&FORMAT=image/png&INFO_FORMAT=text/html&SRS=EPSG:900913&X=585&Y=273");
+//		return ok();
 	}
 	
 	//----------------------------------------------------------------------

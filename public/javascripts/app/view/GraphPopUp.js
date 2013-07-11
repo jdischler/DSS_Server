3d9020f9e851397c092b244715facf6ccc297bdb
Jeffrey Dischler <jdischler.72@gmail.com>
2013-07-11 09:35:18 -0500

    Add hover over tooltips

--- public/javascripts/app/view/GraphPopUp.js
+++ public/javascripts/app/view/GraphPopUp.js
@@ -9,70 +9,77 @@ Ext.define('MyApp.view.GraphPopUp', {
     initComponent: function() {
         var me = this;
 
-        	Ext.define('Habitat_Index', {
-	    extend: 'Ext.data.Model',
-	    fields: ['Freq_Default', 'Freq_Transform', 'Bin']
-	});
+		Ext.define('Habitat_Index', {
+			extend: 'Ext.data.Model',
+			fields: ['Freq_Default', 'Freq_Transform', 'Bin']
+		});
 	
         this.graphstore = Ext.create('Ext.data.Store', {
-	    model: 'Habitat_Index',
-	    data: [
-	    ]
-	});
+			model: 'Habitat_Index',
+			data: []
+		});
                     
         Ext.applyIf(me, {
-            items: [
-                {
-                    xtype: 'chart',
-                    itemId: 'MyGraph',
-                    //height: 250,
-                    //width: 400,
-                    animate: true,
-                    store: this.graphstore,
-                    insetPadding: 20,
-                        legend: {
-				position: 'top'
+            items: [{
+				xtype: 'chart',
+				itemId: 'MyGraph',
+				//height: 250,
+				//width: 400,
+				//animate: true,
+				store: this.graphstore,
+				insetPadding: 20,
+				legend: {
+					position: 'top'
 			    },
-			    tips:
-			    {
-			    	    trackMouse: true,
-			    	    width: 80,
-			    	    height:50,
-			    	    renderer: function(store, item)
-			    	    {
-			    	    	    this.setTitle(store.get('Bin') + '<br />' + store.get('Freq_Default'))
-			    	    }
-			    },
-                    axes: [
-                        {
-                            title: 'Frequency',
-                            type: 'Numeric',
-                            position: 'left',
-                            fields: ['Freq_Default', 'Freq_Transform']
-                        },
-                        {
-			    title: 'Bin',
-			    type: 'Numeric',
-			    position: 'bottom',
-			    fields: ['Bin']
-                        }
-                    ],
-                    series: [
-                        {
-			    type: 'line',
-			    xField: 'Bin',
-			    yField: 'Freq_Default',
-                            smooth: 3
-                        },
-                        {
-			    type: 'line',
-			    xField: 'Bin',
-			    yField: 'Freq_Transform',
-                            smooth: 3
-                        }
-                    ]
-                }
-            ]
+				axes: [{
+					title: 'km\xb2', // square kilometers
+					type: 'Numeric',
+					position: 'left',
+					fields: ['Freq_Default', 'Freq_Transform']
+				},
+				{
+					title: 'Value',
+					type: 'Numeric',
+					position: 'bottom',
+					fields: ['Bin']
+				}],
+				series: [{
+					type: 'line',
+					xField: 'Bin',
+					yField: 'Freq_Default',
+					smooth: 3,
+					tips: {
+						trackMouse: true,
+						width: 120,
+						height: 40,
+						renderer: function(store, item) {
+							var areaUnits = ' km\xb2'; // km2
+							var freq = 'Area: ' + store.get('Freq_Default').toFixed(2) + areaUnits;
+							var bin = 'Value: ' + store.get('Bin').toFixed(3);
+
+							this.setTitle(freq + '<br />' + bin);
+						}
+					}
+				},
+				{
+					type: 'line',
+					xField: 'Bin',
+					yField: 'Freq_Transform',
+					smooth: 3,
+					tips: {
+						trackMouse: true,
+						width: 120,
+						height: 40,
+						renderer: function(store, item) {
+							var areaUnits = ' km\xb2'; // km2
+							var freq = 'Area: ' + store.get('Freq_Transform').toFixed(2) + areaUnits;
+							var bin = 'Value: ' + store.get('Bin').toFixed(3);
+
+							this.setTitle(freq + '<br />' + bin);
+						}
+					}
+				}]
+			}]
         });
 
         me.callParent(arguments);
@@ -80,26 +87,22 @@ Ext.define('MyApp.view.GraphPopUp', {
     
     SetChartData: function(objD, objT)
     {
-
-    	var data1 = objD.Result;
-    	var data2 = objT.Result;
-    	//var data2 = obj.Habitat_Index.Transform;
-    	var Min = objD.Min;
-    	var Max = objD.Max;
-    	
-    	var chart = this.getComponent("MyGraph");
-    	chart.axes.items[1].maximum = Max;
-	chart.axes.items[1].minimum = Min;
-	
-	var array = [];
-	for (var i = 0; i < data1.length; i++)
-	{
-		array.push({ Freq_Default: data1[i]*900/1000000, Freq_Transform: data2[i]*900/1000000, Bin: (Max-Min)/(data1.length) * i + Min });
-	}
-	
-	this.graphstore.loadData(array);
-
-
+		var data1 = objD.Result;
+		var data2 = objT.Result;
+		var Min = objD.Min;
+		var Max = objD.Max;
+		
+		var chart = this.getComponent("MyGraph");
+		chart.axes.items[1].maximum = Max;
+		chart.axes.items[1].minimum = Min;
+		
+		var array = [];
+		for (var i = 0; i < data1.length; i++)
+		{
+			array.push({ Freq_Default: data1[i]*900/1000000, Freq_Transform: data2[i]*900/1000000, Bin: (Max-Min)/(data1.length) * i + Min });
+		}
+		
+		this.graphstore.loadData(array);
     }
 
 });

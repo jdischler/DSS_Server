053d51450ad3de5692974c10e3f4868e5c41c6db
Jeffrey Dischler <jdischler.72@gmail.com>
2013-08-06 15:11:52 -0500

    Add base for getting assumptions data/list from server
    TODO: should read assumptions.dat file

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -107,7 +107,46 @@ public class Application extends Controller
 		// TODO: return NOT ok?
 		return ok();
 	}
+
+	//----------------------------------------------------------------------
+	public static JsonNode createAssumption(String category, String icon,
+						String variableName, 
+						String displayName, float defaultValue) {
 	
+		ObjectNode node = JsonNodeFactory.instance.objectNode();
+		node.put("Category", category);
+		node.put("Icon", icon);
+		node.put("VariableName", variableName);
+		node.put("DisplayName", displayName);
+		node.put("DefaultValue", defaultValue);
+
+		return node;		
+	}
+	
+	// FIXME: TODO: read from ./serverData/assumptions/assumptions.dat
+	//----------------------------------------------------------------------
+	public static Result getAssumptions() throws Exception {
+				
+		ArrayNode array = JsonNodeFactory.instance.arrayNode();
+		array.add(createAssumption("Economic", "", "p_corn", "Corn Price", 120.0f));
+		array.add(createAssumption("Economic", "", "p_grass", "Grass Price", 100.0f));
+		array.add(createAssumption("Economic", "economic_icon.png", "p_eth", "Ethanol Price", 300.0f));
+		
+		array.add(createAssumption("Conversion Rates", "", "conv_corn", "Corn Grain", 1.8f));
+		array.add(createAssumption("Conversion Rates", "", "conv_stover", "Stover", 1.4f));
+		array.add(createAssumption("Conversion Rates", "scenario_icon.png", "conv_grass", "Grass", 1.2f));
+		
+		array.add(createAssumption("Climate", "", "av_temp", "Average Temperature", 76.1f));
+		array.add(createAssumption("Climate", "climate_icon.png", "av_rain", "Average Rainfall", 19.2f));
+		
+		array.add(createAssumption("Other", "policy_icon.png", "temp", "Dunno", 1));
+
+		ObjectNode sendback = JsonNodeFactory.instance.objectNode();
+		sendback.put("Assumptions", array);
+		
+		return ok(sendback);
+	}
+
 	//----------------------------------------------------------------------
 	public static Result Models() throws Exception 
 	{

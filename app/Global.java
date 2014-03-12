2ae2c7819573aff19d72d4a7450752b48c739dda
Jeffrey Dischler <jdischler.72@gmail.com>
2014-03-12 12:28:21 -0500

    Update build version to 51.4

--- app/Global.java
+++ app/Global.java
@@ -17,7 +17,7 @@ public class Global extends GlobalSettings
 	private static final boolean LOAD_DEFAULT_DATA = true;
 	
 	// FIXME: TODO: this will be annoying...investigate automating the version numbering
-	private static final String mServerVersionMessage = "Server version: 0.51.3";
+	private static final String mServerVersionMessage = "Server version: 0.51.4";
 	
 	//--------------------------------------------------------------------------
 	@Override

94c8b5958cdcb68a114a914d879eaec7a3939ef0
Jeffrey Dischler <jdischler.72@gmail.com>
2014-04-14 16:20:34 -0500

    ensure using Ext 4.2.1

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -17,7 +17,7 @@ Ext.Loader.setConfig({
     enabled: true,
 	paths: {
 		GeoExt: "http://geoext.github.com/geoext2/src/GeoExt",
-		Ext: "http://cdn.sencha.io/ext-4.2.1-gpl/src"
+		Ext: "http://cdn.sencha.com/ext/gpl/4.2.1/src"
 	}
 });
 

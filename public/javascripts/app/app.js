4e0fa42f92d9e1746e66f605a43be61aed709f1e
Jeffrey Dischler <jdischler.72@gmail.com>
2015-05-05 15:38:38 -0500

    Import GeoExt and host it on our server

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -16,7 +16,7 @@
 Ext.Loader.setConfig({
     enabled: true,
 	paths: {
-		GeoExt: "https://geoext.github.com/geoext2/src/GeoExt",
+		GeoExt: "./app/GeoExt",
 		//Ext: "http://cdn.sencha.com/ext/gpl/4.2.1/src"
 	}
 });
